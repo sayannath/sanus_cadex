c7148496efce2a419970f04cb5218d0548e125f1
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-01-30 21:05:17 +0530

    up

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import glob
 import re
 import numpy as np
+import joblib
 
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
@@ -12,13 +13,18 @@ from tensorflow.keras.preprocessing import image
 from flask import Flask, redirect, url_for, request, render_template
 
 # Define a flask app
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder='static')
 
+pneumoniamodel = load_model("./model/Pneumonia-DENSENET.h5")         # Necessary
 
-model = load_model("./model/Pneumonia-DENSENET.h5")         # Necessary
+def HeartValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==7):
+        loaded_model = joblib.load(r'./model/heart_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
 
-
-def model_predict(img_path, model):
+def pneumoniamodel_predict(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
 
     # Preprocessing the image
@@ -31,26 +37,28 @@ def model_predict(img_path, model):
     return preds
 
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/pneumonia/', methods=['GET'])
+def pneumonia():
     # Main page
-    return render_template('index.html')
+    return render_template('pneumonia/home.html')
+@app.route('/pneumonia/home.html', methods=['GET'])
+def pneumoniahome():
+    # Main page
+    return render_template('pneumonia/home.html')
 
-@app.route('/base.html',methods=['GET'])
-def base():
-    return render_template('base.html')
-
-@app.route('/index.html',methods=['GET'])
-def home():
-    return render_template('index.html')
+@app.route('/pneumonia/test',methods=['GET'])
+def pneumoniatest():
+    return render_template('pneumonia/predict.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/pneumonia/predict', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
+    if request.method == 'POST': 
+        print("hanji-------------------------")       
+ 
         # Get the file from post request
         f = request.files['file']
- 
+       
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
@@ -58,7 +66,7 @@ def upload():
         f.save(file_path)
 
         # Make prediction
-        preds = model_predict(file_path, model)
+        preds = pneumoniamodel_predict(file_path, pneumoniamodel)
 
         # Process your result for human
         # pred_class = preds.argmax(axis=-1)            # Simple argmax
@@ -71,6 +79,25 @@ def upload():
         return result
     return None
 
+@app.route("/heart")
+def cancer():
+    return render_template("heart/index.html")
+
+@app.route('/heart/predict', methods = ["POST"])
+def predict():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+         #diabetes
+        if(len(to_predict_list)==7):
+            result = HeartValuePredictor(to_predict_list,7)
+    
+    if(int(result)==1):
+        prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
+    else:
+        prediction = "No need to fear. You have no dangerous symptoms of the disease"
+    return(render_template("heart/result.html", prediction_text=prediction))       
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
--- static/pneumonia/js/main.js
+++ static/pneumonia/js/main.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
     // Make prediction by calling api /predict
     $.ajax({
       type: "POST",
-      url: "/predict",
+      url: "/pneumonia/predict",
       data: form_data,
       contentType: false,
       cache: false,
